8cd654f4cc71df5b52c04eb25a4b889f97e35fac
woraseth <woraseth@users.noreply.github.com>
2015-09-22 15:53:26 +0700

    The Decoder

--- P458.java
+++ P458.java
@@ -1,22 +1,19 @@
 // 458 - The Decoder
-// WA
+// has to use DataInputStream
 
-import java.util.Scanner;
+import java.io.DataInputStream;
 
 public class Main {
 
-  static final Scanner sc = new Scanner(System.in);
-
-  public static void main(String[] args) {
-    while (sc.hasNextLine()) {
-      String s = sc.nextLine();
-      StringBuilder sb = new StringBuilder();
-      for (int i = 0; i < s.length(); i++) {
-        char c = s.charAt(i);
-        c -= 7;
-        sb.append(c);
+  public static void main(String[] args) throws Exception {
+    DataInputStream dis = new DataInputStream(System.in);
+    int i;
+    while ((i = dis.read()) != -1) {
+      if (i != '\n') {
+        System.out.print((char) (i - 7));
+      } else {
+        System.out.println();
       }
-      System.out.println(sb);
     }
 
   }
