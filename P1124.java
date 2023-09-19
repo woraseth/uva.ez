efa11e2dc38d3be2cadedd48f996a42bbf9ced06
woraseth <woraseth@users.noreply.github.com>
2023-09-19 09:58:02 +0700

    Create P1124.java

---
+++ P1124.java
@@ -0,0 +1,12 @@
+// 1124 - Celebrity jeopardy
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws Exception {
+    while (sc.hasNextLine()) {
+      System.out.println(sc.nextLine());
+    }
+  }
+}
