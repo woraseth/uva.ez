97b185d9a47ff1fcf5987620c7db3772de852324
woraseth <woraseth@users.noreply.github.com>
2023-09-28 09:21:42 +0700

    Create P10963.java

---
+++ P10963.java
@@ -0,0 +1,26 @@
+// 10963 The Swallowing Ground
+
+import java.util.*;
+
+public class Main {
+  static Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws Exception {
+    // sc = new Scanner(new FileInputStream("c:\\temp\\in.txt"));
+    int testcase = sc.nextInt();
+    for (int t = 0; t < testcase; t++) {
+      int width = sc.nextInt();
+      int max = Integer.MIN_VALUE;
+      int min = Integer.MAX_VALUE;
+      for (int i = 0; i < width; i++) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        max = Math.max(max, a-b);
+        min = Math.min(min, a-b);
+      }
+      if (max == min) 
+        System.out.println("yes");
+      else
+        System.out.println("no");
+    }
+  }
+}
