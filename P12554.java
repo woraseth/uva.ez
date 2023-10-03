20de3e32c1d9a09a161c16d42e904fc595cf871c
woraseth <woraseth@users.noreply.github.com>
2023-10-03 09:12:49 +0700

    Create P12554.java

---
+++ P12554.java
@@ -0,0 +1,32 @@
+// 12554 A Special “Happy Birthday” Song!!!
+
+import java.util.*;
+
+public class Main {
+  static Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws Exception {
+    String lyric = "Happy birthday to you Happy birthday to you Happy birthday to Rujia Happy birthday to you";
+    String[] a = lyric.split(" ");
+
+    // sc = new Scanner(new FileInputStream("c:\\temp\\in.txt"));
+    int n = sc.nextInt();
+    String[] people = new String[n];
+    boolean[] sing = new boolean[n];
+    for (int i = 0; i < n; i++) {
+      people[i] = sc.next();
+    }
+
+    while (true) {
+      int pos = 0;
+      for (int i = 0; i < a.length; i++) {
+        System.out.printf("%s: %s%n", people[pos], a[i]);
+        sing[pos] = true;
+        pos++;
+        if (pos == people.length) pos = 0;
+      }
+      boolean allSing = true;
+      for (boolean b : sing) if (b == false) allSing = false;
+      if (allSing) break;
+    }
+  }
+}
