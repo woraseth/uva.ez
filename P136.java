d469d47ce42defaa4a50d8c9c7579f60f5fce573
woraseth <woraseth@users.noreply.github.com>
2024-03-20 10:14:38 +0700

    Create P136.java

---
+++ P136.java
@@ -0,0 +1,30 @@
+
+// 136 Ugly Number
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // List<Long> list = new ArrayList<>();
+        // list.add(1L);
+        // long n = 2;
+        // int N = 1499;
+        // while (list.size() != 11) {
+        //     long t = n;
+        //     while (t % 2 == 0) {
+        //         t /= 2;
+        //     }
+        //     while (t % 3 == 0) {
+        //         t /= 3;
+        //     }
+        //     while (t % 5 == 0) {
+        //         t /= 5;
+        //     }
+        //     if (t == 1) {
+        //         list.add(n);
+        //     }
+        //     n++;
+        // }
+        // System.out.println(list.get(N-1));
+        System.out.printf("The 1500'th ugly number is %d.%n", 859963392);
+    }
+}
